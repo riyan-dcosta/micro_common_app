7819bdb95294a874a9ea9afa3ca49f012ae7f158
riyan-dcosta <rdriyan@gmail.com>
2023-08-10 14:47:52 +0530

    theme updates

--- lib/components/component_base_theme.dart
+++ lib/components/component_base_theme.dart
@@ -14,3 +14,68 @@ class MaThemeData extends StatelessWidget {
     );
   }
 }
+
+abstract class MaCustomAbstract extends StatelessWidget {
+  const MaCustomAbstract({super.key});
+
+  Widget customWidget(BuildContext context);
+}
+
+class MaCustom extends MaCustomAbstract {
+  const MaCustom({required this.child, super.key});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: MaTheme.maLightTheme,
+      child: child,
+    );
+  }
+
+  @override
+  Widget customWidget(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
+
+
+///
+// class AppTheme {
+//   final Color background;
+//   final Color foreground;
+//   final int padding;
+//
+//   AppTheme(this.background, this.foreground, this.padding);
+// }
+//
+// class CustomTheme extends InheritedWidget {
+//   final Widget child;
+//   final CThemeState data;
+//   final AppTheme theme;
+//
+//   CustomTheme(this.child, this.data, this.theme);
+//
+//
+//   @override
+//   bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+//     return true;
+//   }
+// }
+//
+// class CTheme extends StatefulWidget {
+//   const CTheme({super.key, required this.child, required this.theme});
+//   final Widget child;
+//   final AppTheme theme;
+//
+//   @override
+//   State<CTheme> createState() => CThemeState();
+// }
+//
+// class CThemeState extends State<CTheme> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
